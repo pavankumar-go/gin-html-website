215d237662700d20221d912299cc138a31c7c8cc
pavan.kumar9 <pavan.kumar@freighttiger.com>
2021-10-13 21:26:40 +0530

    fixed mobile image view, updated about page

--- scripts/image_view.js
+++ scripts/image_view.js
@@ -1,17 +1,32 @@
 const enlarge = document.querySelectorAll('.images');
 const allImages = document.querySelectorAll('.container');
+
 const imageView = document.getElementById('image-view');
 const mainBody = document.getElementById('main-body');
 
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
+
 const imageBox = document.querySelector('.image-box');
+const imgBox = document.getElementById('img-box');
 const imgTag = document.getElementById('image-tag');
 
-// const placeID = document.getElementById('placeID');
+const navBar = document.querySelector('.nav-bar');
+
 
 let currentImgIndex = 0;
 
+// for mobile phones - touch on image to navigate between images
+imgTag.addEventListener('click', function () {
+    currentImgIndex++;
+    if (currentImgIndex === allImages.length) {
+        currentImgIndex = 1;
+    }
+
+    currentImageDisplay(currentImgIndex);
+})
+
+// left arrow - click to go previous image from current image
 prevBtn.addEventListener('click', function () {
     currentImgIndex--;
     if (currentImgIndex === 0) {
@@ -21,6 +36,7 @@ prevBtn.addEventListener('click', function () {
     currentImageDisplay(currentImgIndex);
 })
 
+// right arrow - click to go next image from current image
 nextBtn.addEventListener('click', function () {
     currentImgIndex++;
     if (currentImgIndex === allImages.length) {
@@ -30,13 +46,25 @@ nextBtn.addEventListener('click', function () {
     currentImageDisplay(currentImgIndex);
 })
 
-
+// get all images and view individual on click
 enlarge.forEach(function (btn, index) {
     btn.addEventListener('click', function () {
+        navBar.style.display = "none";
         imageView.style.display = "block";
         imageBox.style.display = "block";
-        nextBtn.style.display = "block";
-        prevBtn.style.display = "block";
+
+        // disable arrow keys for screens less than 820px (ipad onwards)
+        if (window.screen.width <= 820) {
+            var deviceWidth = window.screen.width - 20;
+            // set width to device screen size - 20px
+            imgBox.style.width = deviceWidth + "px";
+            nextBtn.style.display = "none";
+            prevBtn.style.display = "none";
+        }else {
+            nextBtn.style.display = "block";
+            prevBtn.style.display = "block";
+        }
+
         currentImgIndex = index + 1;
         currentImageDisplay(currentImgIndex);
     })
@@ -44,15 +72,16 @@ enlarge.forEach(function (btn, index) {
 
 function currentImageDisplay(index) {
     imgTag.src = `/static/assets/images/places/1/${index}.jpg`
-    // imageBox.style.background = `url(/static/assets/images/places/1/${index}.jpg) center/cover no-repeat`
 }
 
+// exist single image view by clicking anywhere other than the image or arrow keys
+// TODO: add X icon if needed.
 mainBody.addEventListener('click', function (e) {
     if (e.target.id == "image-view") {
-        console.log(e.target.id)
         imageView.style.display = "none";
         imageBox.style.display = "none";
         nextBtn.style.display = "none";
         prevBtn.style.display = "none";
+        navBar.style.display = "block";
     }
 })
