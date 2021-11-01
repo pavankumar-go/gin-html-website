b8e8c579af62de27bba3b80565e70b429c29fb03
pavan.kumar9 <pavan.kumar@freighttiger.com>
2021-11-01 18:47:11 +0530

    added secret loading during boot

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -1,20 +1,31 @@
 package middleware
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		admPass := os.Getenv("ADMIN_PASS")
 		reqPass, ok := c.GetPostForm("password")
+		log.Println(admPass, "---", reqPass)
 		if !ok || reqPass != admPass {
+			log.Println("auth failed")
 			c.AbortWithStatusJSON(200, "invalid request")
 			return
 		}
 
 		c.Next()
+	}
+}
+
+func init() {
+	err := godotenv.Load(os.Getenv("SECRET_PATH"))
+	if err != nil {
+		log.Fatalln("failed to load env: ", err)
 	}
 }
