9940562aa6bea995d3a91d6fc7055dfadb641e38
pavan.kumar9 <pavan.kumar@freighttiger.com>
2021-11-02 22:42:18 +0530

    puts admin api ui to new file

---
+++ app/handler/admin_frontend.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var Host string
+
+func init() {
+	var ok bool
+	Host, ok = os.LookupEnv("HOST")
+	if !ok {
+		log.Fatalln("HOST is unset")
+	}
+}
+
+// Admin frontend
+func AdminAPIBirdUpload() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(200, "bird_upload.html", gin.H{
+			"Host": Host,
+		})
+	}
+}
+
+func AdminAPIPlaceUpload() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(200, "place_upload.html", gin.H{
+			"Host": Host,
+		})
+	}
+}
+
+func AdminAPIPlacePatch() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(200, "update_place_thumbnail.html", gin.H{
+			"Host": Host,
+		})
+	}
+}
